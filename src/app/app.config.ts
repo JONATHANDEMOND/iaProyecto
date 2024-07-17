15c13595350d98d657a9b76055d3d0780e854749
JONATHANDEMOND <133927721+JONATHANDEMOND@users.noreply.github.com>
2024-07-16 22:14:49 -0500

    cambios

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -7,5 +7,5 @@ import { provideHttpClient, withFetch } from '@angular/common/http';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideClientHydration(),
-    provideHttpClient()]
+    provideHttpClient(withFetch()) ]
 };
